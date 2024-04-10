33143f4658a2fc920714944950db0d74aacffb13
Timber <timber@systemli.org>
2024-04-10 12:16:52 +0000

    Create badge: AI-Tool doesn't work #160 (#49)
    * Decode url safe base64 string
    
    * Fix replacement
    
    ---------
    
    Co-authored-by: Timber <timber@opensenselab.org>

--- apps/mainsite/views.py
+++ apps/mainsite/views.py
@@ -88,6 +88,8 @@ def upload(req):
 @authentication_classes([TokenAuthentication, SessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
 def aiskills(req, searchterm):
+    # The searchterm is encoded URL safe, meaning that + and / got replaced by - and _
+    searchterm = searchterm.replace("-", "+").replace("_", "/")
     searchterm = base64.b64decode(searchterm).decode("utf-8")
     if req.method != 'GET':
         return JsonResponse({"error": "Method not allowed"}, status=status.HTTP_400_BAD_REQUEST)
