3219897b3d6a8f38c5ebbea91f737cec8679f2c8
jona159 <65068389+jona159@users.noreply.github.com>
2024-05-21 08:44:37 +0200

    Fix/password reset (#109)
    * fix: reset password error

--- apps/badgeuser/api.py
+++ apps/badgeuser/api.py
@@ -353,7 +353,11 @@ class BadgeUserForgotPassword(BaseUserRecoveryView):
         except DjangoValidationError as e:
             return Response(dict(password=e.messages), status=HTTP_400_BAD_REQUEST)
 
-        cache.delete(backoff_cache_key(user.email))
+        # use delete many due to an incompatibility with python-memcached and django v3.2
+        # TODO: maybe replace python-memcached with pylibmc  
+        cache.delete_many([backoff_cache_key(user.email)])
+        # cache.delete(backoff_cache_key(user.email))
+
 
         user.set_password(password)
         user.save()
