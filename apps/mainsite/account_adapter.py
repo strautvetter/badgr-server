a438d7b95effe0e852cbd516330b74bc95be5a5e
jona159 <65068389+jona159@users.noreply.github.com>
2024-07-11 12:37:21 +0200

    Fix/pdf competencies (#186)
    * fix: pdf permissions
    
    * feat: rework competencies in pdf
    
    * fix: link
    
    * fix: competency layout and studyload in pdf
    
    * fix: dont display hours unless greater than two hours

--- apps/mainsite/account_adapter.py
+++ apps/mainsite/account_adapter.py
@@ -140,10 +140,11 @@ def generate_pdf_content(slug):
                         Story.append(Spacer(1, 20)) 
 
                     studyload = "%s Minuten" % competencies[i]['studyLoad']
-                    if competencies[i]['studyLoad'] > 60:
-                        studyload = "%s Stunden" % competencies[i]['studyLoad']
+                    if competencies[i]['studyLoad'] > 120:
+                        studyload = "%s Stunden" % int(competencies[i]['studyLoad'] / 60 )
                     competency = competencies[i]['name']
-                    rounded_rect = RoundedRectFlowable(0, -15, 120, 45, 15, text=competency, strokecolor="#492E98", fillcolor="#F5F5F5", studyload = studyload, esco = competencies[i]['escoID'])
+                    rounded_rect = RoundedRectFlowable(0, -1, 450, 45, 10, text=competency, strokecolor="#492E98", fillcolor="#F5F5F5", studyload = studyload, esco = competencies[i]['escoID'])
+
                     Story.append(rounded_rect)    
                     Story.append(Spacer(1, 20))   
                     
