b23d0fed4feb789c86c177e37aaf44252449c416
jona159 <65068389+jona159@users.noreply.github.com>
2024-07-18 09:50:42 +0200

    Feat/replace oeb logo (#192)
    * feat: replace logo
    
    * feat: replace logo, adjust logo in pdf

--- apps/backpack/views.py
+++ apps/backpack/views.py
@@ -122,8 +122,8 @@ def AllPageSetup(canvas, doc):
         canvas.drawPath(path, fill=1, stroke=1)
 
     # Header
-    logo = ImageReader("{}images/Logo-New.png".format(settings.STATIC_URL))
-    canvas.drawImage(logo, 10, 650, width=200, height=200, mask="auto", preserveAspectRatio=True)
+    logo = ImageReader("{}images/Logo-Oeb.png".format(settings.STATIC_URL))
+    canvas.drawImage(logo, 20, 675, width=150, height=150, mask="auto", preserveAspectRatio=True)
     page_width = canvas._pagesize[0]
     canvas.setStrokeColor("#492E98")
     canvas.line(page_width / 2 - 100, 750, page_width / 2 + 250, 750)
