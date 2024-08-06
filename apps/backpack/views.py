946e1ce7b8704532effdc49ca6e5ba718647fe4b
jona159 <65068389+jona159@users.noreply.github.com>
2024-08-06 16:05:25 +0200

    fix: esco link spacing (#215)
    Co-authored-by: Yousef Qamaz <40634685+YouQam@users.noreply.github.com>

--- apps/backpack/views.py
+++ apps/backpack/views.py
@@ -240,7 +240,7 @@ def create_multi_page(response, first_page_content, competencies, name, badge_na
               Story.append(Spacer(1, 20))   
                  
             if esco: 
-                Story.append(Spacer(1, 100))
+                Story.append(Spacer(1, 25))
                 text_style = ParagraphStyle(name='Text_Style', fontSize=12, leading=20, alignment=TA_LEFT)
                 link_text = '<span><i>(E) = Kompetenz nach ESCO (European Skills, Competences, Qualifications and Occupations) <br/>' \
                     'Die Kompetenzbeschreibungen gemäß ESCO sind abrufbar über <a color="blue" href="https://esco.ec.europa.eu/de">https://esco.ec.europa.eu/de</a>.</i></span>'
