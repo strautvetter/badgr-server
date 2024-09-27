44fa310ed2e62a884494cf5d632630fda45e5a28
jona159 <65068389+jona159@users.noreply.github.com>
2024-09-27 11:43:54 +0200

    fix: isser no image leads to error (#248)

--- apps/mainsite/views.py
+++ apps/mainsite/views.py
@@ -253,11 +253,12 @@ def PageSetup(canvas, doc, badgeImage, issuerImage):
 
     # Header
     try:
-        institutionImage = ImageReader(issuerImage)
-        canvas.drawImage(institutionImage, 20, 705, width=80, height=80, mask="auto", preserveAspectRatio=True)
-    except FileNotFoundError:
+        if issuerImage is not None: 
+            institutionImage = ImageReader(issuerImage)
+            canvas.drawImage(institutionImage, 20, 705, width=80, height=80, mask="auto", preserveAspectRatio=True)
+    except:
         oebLogo = ImageReader("{}images/Logo-Oeb.png".format(settings.STATIC_URL))  
-        canvas.drawImage(oebLogo, 20, 705, width=80, height=80, mask="auto", preserveAspectRatio=True)
+        canvas.drawImage(oebLogo, 20, 710, width=80, height=80, mask="auto", preserveAspectRatio=True)
 
     page_width = canvas._pagesize[0]
     page_height = canvas._pagesize[1]
