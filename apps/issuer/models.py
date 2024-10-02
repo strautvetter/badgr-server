4530eb70cdc99cab0856231c7f0be06602164e31
jona159 <65068389+jona159@users.noreply.github.com>
2024-10-02 10:39:34 +0200

    fix: remove special chars from site_name (#251)
    * fix: remove special chars from site_name
    
    * fix: only substitute @ symbol

--- apps/issuer/models.py
+++ apps/issuer/models.py
@@ -583,7 +583,7 @@ class Issuer(ResizeUploadedImage,
                 'issuer_name': re.sub(r'[^\w\s]+', '', self.name, 0, re.I),
                 'issuer_url': self.url,
                 'issuer_email': self.email,
-                'site_name': badgr_app.name,
+                'site_name': re.sub(r'@', '', badgr_app.name),
                 'badgr_app': badgr_app
             }
         except KeyError as e:
